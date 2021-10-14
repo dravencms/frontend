8858dfe2edeb9ea614c473b3057b11d7fb413055
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-14 21:22:17 +0200

    chore: Cleanup

--- src/Dravencms/FrontModule/BasePresenter.php
+++ src/Dravencms/FrontModule/BasePresenter.php
@@ -6,11 +6,34 @@
 namespace Dravencms\FrontModule;
 
 
+use Dravencms\Frontend\Frontend;
+use Dravencms\Frontend\ITemplate;
+
 class BasePresenter extends \Dravencms\BasePresenter
 {
-    public function startup()
+    /** @var Frontend */
+    public $frontend;
+
+    public function startup(): void
     {
         $this->getUser()->getStorage()->setNamespace('Front');
         parent::startup();
+    }
+
+    public function formatLayoutTemplateFiles(): array
+    {
+        $list = parent::formatLayoutTemplateFiles();
+        $layout = $this->getLayoutName();
+        if ($found = $this->frontend->findTemplate($layout)) {
+            $list[] = $found->getPath();
+        }
+    }
+
+    /**
+     * @return \Dravencms\Base\ITemplate|null
+     */
+    public function getCurrentTemplate(): ?ITemplate
+    {
+        return $this->frontend->findTemplate($this->getLayoutName());
     }
 }
